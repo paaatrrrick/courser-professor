0acfce0052fb25b75bd4dec645a4a2558c295926
liaozhu <liao-zhu@uiowa.edu>
2023-10-23 01:46:23 -0500

    hehehaha

--- client/src/app/components/ChatMessage.jsx
+++ client/src/app/components/ChatMessage.jsx
@@ -7,11 +7,15 @@ function ChatMessage({ message, isUser }) {
       }`}
     >
       <div
-        className={`px-4 py-2 flex justify-between min-w-3/4 max-w-5/6 items-center shrink ${
-          isUser ? "bg-bucksBlue rounded-l-lg rounded-tr-lg" : "bg-zinc-500 rounded-r-lg rounded-tl-lg"
+        className={`px-4 py-2 flex justify-between min-w-3/4 max-w-5/6 items-center shrink overflow-auto ${
+          isUser
+            ? "bg-bucksBlue rounded-l-lg rounded-tr-lg"
+            : "bg-zinc-500 rounded-r-lg rounded-tl-lg"
         }`}
       >
-        <p className="break-words text-white text-md w-full">{message}</p>
+        <p className="break-words text-white text-md w-full whitespace-no-wrap overflow-hidden">
+          {message}
+        </p>
       </div>
     </div>
   );
